055c9018ed9d78f220bf86710395a8dfe6079d3c
Sergio Ruiz <serginator@gmail.com>
2024-09-05 12:43:06 +0200

    updated logic on times

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,33 @@
 */
 
 #include <M5Unified.h>
+#include <M5Cardputer.h>
 #include <USB.h>
 #include <USBHIDMouse.h>
 
 USBHIDMouse Mouse;
 
 unsigned long lastMoveTime = 0;
-const unsigned long MIN_DELAY_MS = 240000; // 4 minutes
-const unsigned long MAX_DELAY_MS = 300000; // 5 minutes
+unsigned long MIN_DELAY_MS = 60000;  // 1 minute
+unsigned long MAX_DELAY_MS = 300000; // 5 minutes
 unsigned long currentDelay = MIN_DELAY_MS;
+bool jiggling = false;
+
+void updateDisplay() {
+  M5.Display.fillScreen(BLACK);
+  M5.Display.setCursor(0, 0);
+  M5.Display.setTextSize(2);
+  M5.Display.println("Mouse Jiggler");
+  M5.Display.println("Min: " + String(MIN_DELAY_MS / 60000) + " min");
+  M5.Display.println("Max: " + String(MAX_DELAY_MS / 60000) + " min");
+  M5.Display.println("Up/Down: Change Min");
+  M5.Display.println("Left/Right: Change Max");
+  M5.Display.println("Press OK to Jiggle");
+}
 
 void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
-
-  M5.Display.setTextSize(2);
-  M5.Display.println("M5Cardputer Mouse Jiggler");
-  M5.Display.println("starting...");
 
   USB.begin();
   Mouse.begin();
@@ -34,31 +44,75 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Mouse Jiggler initialized");
 
-  randomSeed(analogRead(0));
+  updateDisplay();
 }
 
 void loop() {
   M5.update();
 
-  unsigned long currentTime = millis();
+  if (!jiggling) {
+    if (M5Cardputer.Keyboard.isKeyPressed(';')) {  // Up button
+      MIN_DELAY_MS = MIN_DELAY_MS + 60000;
+      if (MIN_DELAY_MS > MAX_DELAY_MS) {
+        MIN_DELAY_MS = MAX_DELAY_MS;
+      }
+      updateDisplay();
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed('.')) {  // Down button
+      MIN_DELAY_MS = MIN_DELAY_MS - 60000;
+      if (MIN_DELAY_MS < 0) {
+        MIN_DELAY_MS = 0;
+      }
+      updateDisplay();
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed(',')) {  // Left button
+      MAX_DELAY_MS = MAX_DELAY_MS - 60000;
+      if (MAX_DELAY_MS < MIN_DELAY_MS) {
+        MAX_DELAY_MS = MIN_DELAY_MS;
+      }
+      updateDisplay();
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed('/')) {  // Right button
+      MAX_DELAY_MS = MAX_DELAY_MS + 60000;
+      if (MAX_DELAY_MS < MIN_DELAY_MS) {
+        MAX_DELAY_MS = MIN_DELAY_MS + 60000;
+      }
+      updateDisplay();
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed(0x28)) {  // ENTER button
+      jiggling = true;
+      M5.Display.fillScreen(BLACK);
+      M5.Display.setCursor(0, 0);
+      M5.Display.println("Jiggling...");
+      M5.Display.println("Min: " + String(MIN_DELAY_MS / 60000) + " min");
+      M5.Display.println("Max: " + String(MAX_DELAY_MS / 60000) + " min");
+      M5.Display.println("Press OK to Stop");
+    }
+  } else {
+    unsigned long currentTime = millis();
 
-  if (currentTime - lastMoveTime >= currentDelay) {
-    Mouse.move(2, 0);
-    delay(500);
-    Mouse.move(-2, 0);
-    lastMoveTime = currentTime;
+    if (currentTime - lastMoveTime >= currentDelay) {
+      Mouse.move(2, 0);
+      delay(500);
+      Mouse.move(-2, 0);
+      lastMoveTime = currentTime;
 
-    currentDelay = random(MIN_DELAY_MS, MAX_DELAY_MS + 1);
+      currentDelay = random(MIN_DELAY_MS, MAX_DELAY_MS + 1);
 
-    M5.Display.fillScreen(BLACK);
-    M5.Display.setCursor(0, 0);
-    M5.Display.println("Mouse moved!");
-    M5.Display.print("Next move in: ");
-    M5.Display.print(currentDelay / 1000);
-    M5.Display.println(" seconds");
-    Serial.println("Mouse moved");
-    Serial.print("Next move in: ");
-    Serial.print(currentDelay / 1000);
-    Serial.println(" seconds");
+      M5.Display.fillScreen(BLACK);
+      M5.Display.setCursor(0, 0);
+      M5.Display.println("Mouse moved!");
+      M5.Display.print("Next move in: ");
+      M5.Display.print(currentDelay / 1000);
+      M5.Display.println(" seconds");
+      Serial.println("Mouse moved");
+      Serial.print("Next move in: ");
+      Serial.print(currentDelay / 1000);
+      Serial.println(" seconds");
+    }
+    if (M5Cardputer.Keyboard.isKeyPressed(0x28)) {  // ENTER button
+      jiggling = false;
+      updateDisplay();
+    }
   }
 }
