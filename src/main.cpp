91c41cc1b0512a236807d89c77c7fe08e2b962a6
Sergio Ruiz <serginator@gmail.com>
2024-09-05 12:58:20 +0200

    new fix to know the state of the pressed keys

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@ unsigned long MAX_DELAY_MS = 300000; // 5 minutes
 unsigned long currentDelay = MIN_DELAY_MS;
 bool jiggling = false;
 
+bool upPressed = false, downPressed = false, leftPressed = false, rightPressed = false, enterPressed = false;
+
 void updateDisplay() {
   M5Cardputer.Display.fillScreen(BLACK);
   M5Cardputer.Display.setCursor(0, 0);
@@ -28,8 +30,8 @@ void updateDisplay() {
   M5Cardputer.Display.println("Mouse Jiggler");
   M5Cardputer.Display.println("Min: " + String(MIN_DELAY_MS / 60000) + " min");
   M5Cardputer.Display.println("Max: " + String(MAX_DELAY_MS / 60000) + " min");
-  M5Cardputer.Display.println("Up/Down: Change Min");
-  M5Cardputer.Display.println("Left/Right: Change Max");
+  M5Cardputer.Display.println("Up/Down: +- Min");
+  M5Cardputer.Display.println("Left/Right: +-Max");
   M5Cardputer.Display.println("Press OK to Jiggle");
 }
 
@@ -48,23 +50,39 @@ void loop() {
   M5Cardputer.update();
 
   if (!jiggling) {
-    if (M5Cardputer.Keyboard.isKeyPressed(';')) {  // Up button
+    if (M5Cardputer.Keyboard.isKeyPressed(';') && !upPressed) {  // Up button
       MIN_DELAY_MS = min(MAX_DELAY_MS - 60000, MIN_DELAY_MS + 60000);
       updateDisplay();
+      upPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed(';')) {
+      upPressed = false;
     }
-    if (M5Cardputer.Keyboard.isKeyPressed('.')) {  // Down button
+
+    if (M5Cardputer.Keyboard.isKeyPressed('.') && !downPressed) {  // Down button
       MIN_DELAY_MS = max(60000UL, MIN_DELAY_MS - 60000);
       updateDisplay();
+      downPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed('.')) {
+      downPressed = false;
     }
-    if (M5Cardputer.Keyboard.isKeyPressed(',')) {  // Left button
+
+    if (M5Cardputer.Keyboard.isKeyPressed(',') && !leftPressed) {  // Left button
       MAX_DELAY_MS = max(MIN_DELAY_MS + 60000, MAX_DELAY_MS - 60000);
       updateDisplay();
+      leftPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed(',')) {
+      leftPressed = false;
     }
-    if (M5Cardputer.Keyboard.isKeyPressed('/')) {  // Right button
+
+    if (M5Cardputer.Keyboard.isKeyPressed('/') && !rightPressed) {  // Right button
       MAX_DELAY_MS = min(3600000UL, MAX_DELAY_MS + 60000);
       updateDisplay();
+      rightPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed('/')) {
+      rightPressed = false;
     }
-    if (M5Cardputer.Keyboard.isKeyPressed(0x28)) {  // ENTER button
+
+    if (M5Cardputer.Keyboard.isKeyPressed(0x28) && !enterPressed) {  // ENTER button
       jiggling = true;
       M5Cardputer.Display.fillScreen(BLACK);
       M5Cardputer.Display.setCursor(0, 0);
@@ -72,6 +90,9 @@ void loop() {
       M5Cardputer.Display.println("Min: " + String(MIN_DELAY_MS / 60000) + " min");
       M5Cardputer.Display.println("Max: " + String(MAX_DELAY_MS / 60000) + " min");
       M5Cardputer.Display.println("Press OK to Stop");
+      enterPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed(0x28)) {
+      enterPressed = false;
     }
   } else {
     unsigned long currentTime = millis();
@@ -95,9 +116,12 @@ void loop() {
       Serial.print(currentDelay / 1000);
       Serial.println(" seconds");
     }
-    if (M5Cardputer.Keyboard.isKeyPressed(0x28)) {  // ENTER button
+    if (M5Cardputer.Keyboard.isKeyPressed(0x28) && !enterPressed) {  // ENTER button to stop
       jiggling = false;
       updateDisplay();
+      enterPressed = true;
+    } else if (!M5Cardputer.Keyboard.isKeyPressed(0x28)) {
+      enterPressed = false;
     }
   }
 }
